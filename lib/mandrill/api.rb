8e2dd2682376a2e09ef7222a6f2096c7abef7aeb
Kevin Bongart <contact@kevinbongart.net>
2012-10-17 16:11:00 -0400

    Fix API calls with array of parameters.

--- lib/mandrill/api.rb
+++ lib/mandrill/api.rb
@@ -4,12 +4,12 @@ module Mandrill
     instance_methods.each do |m|
       undef_method m unless m.to_s =~ /^__|object_id|method_missing|respond_to?|to_s|inspect|kind_of?|should|should_not/
     end
-    
+
     # Mandrill API Documentation: http://mandrillapp.com/api/docs
     API_VERSION = "1.0"
     API_URL = "https://mandrillapp.com/api"
     AUTH_URL = "https://mandrillapp.com/api-auth/"
-    
+
     # Generate a Mandrill +authorization_url+.
     # Returns a URL to redirect users to so that they will be prompted
     # to enter their Mandrill username and password to authorize a
@@ -30,7 +30,7 @@ module Mandrill
     def self.authorization_url(app_id, redirect_url)
       "#{AUTH_URL}?id=#{app_id}&redirect_url=#{URI.escape(redirect_url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
     end
-    
+
     # Initialize
     def initialize(api_key, config = {})
       defaults = {
@@ -40,7 +40,7 @@ module Mandrill
       @config = defaults.merge(config).freeze
       @api_key = api_key
     end
-    
+
     # Dynamically find API methods
     def method_missing(api_method, *args) # :nodoc:
       call(api_method, *args)
@@ -51,26 +51,26 @@ module Mandrill
         raise(API::Error.new(JSON.parse(@response.body)["code"], JSON.parse(@response.body)["message"]))
       end
     end
-    
+
     # Check the API to see if a method is supported
     def respond_to?(api_method, *args) # :nodoc:
       call(api_method, *args)
       @response.code == 500 ? false : true
     end
-    
+
     # Display the supported methods
     def public_methods # :nodoc:
       [:messages, :senders, :tags, :templates, :urls, :users]
     end
-    
+
     # Call the API
     def call(api_method, *args)
       req_endpoint = "#{API_URL}/#{@config[:api_version]}/#{api_method.to_s}/#{args.first.to_s}.#{@config[:format]}"
       req_body = {:key => @api_key}
       req_body.merge!(args.last) if args.last.is_a?(Hash)
-      @response = HTTPI.post(req_endpoint, req_body)
+      @response = HTTPI.post(req_endpoint, req_body.to_json.to_s)
     end
-    
+
     class Error < StandardError
       def initialize(code, message)
         super "(#{code}) #{message}"
